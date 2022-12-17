1657395ec616cdb4dd29f1248090ca006ba29b32
StatTrakR0 <59964937+StatTrakR0@users.noreply.github.com>
2022-12-17 18:47:03 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# test-upload
+# Clone of Xbox Game Catalog
+
+I showed how to build catalog using [Next.js](https://nextjs.org) and [Tailwind CSS](https://tailwindcss.com).
+
+# Клон сайта Xbox Game Catalog
+
+Я показывал как собрать каталог игр на примере магазина Xbox [на вебинаре](https://www.youtube.com/watch?v=byjGtCn9f08&t=15s)
---
+++ src/pages/[[...path]].jsx
@@ -0,0 +1,57 @@
+import { fetchProducts } from 'lib/products'
+import Catalog from 'components/Catalog'
+
+export async function getServerSideProps(ctx) {
+	const products = await fetchProducts({
+		filter: ctx.params.path?.[0],
+		available: ctx.query.available,
+	})
+
+	return {
+		props: {
+			products,
+		},
+	}
+}
+
+export default function Index({ products }) {
+	return (
+		<>
+			<Catalog products={products} />
+			<footer className="mt-4 p-6 text-center leading-tight space-y-2">
+				<p>
+					Made with ❤️ by{' '}
+					<a
+						href="https://pavel.mineev.me"
+						className="inline-block border-b border-sky-200 text-sky-600"
+					>
+						Pavel Mineev
+					</a>
+				</p>
+				<p>
+					Powered by{' '}
+					<a
+						href="https://nextjs.org"
+						className="inline-block border-b border-sky-200 text-sky-600"
+					>
+						Next.js
+					</a>{' '}
+					and{' '}
+					<a
+						href="https://tailwindcss.com"
+						className="inline-block border-b border-sky-200 text-sky-600"
+					>
+						Tailwind CSS
+					</a>
+					. Hosted on ▲
+					<a
+						href="https://vercel.com"
+						className="inline-block border-b border-sky-200 text-sky-600"
+					>
+						Vercel
+					</a>
+				</p>
+			</footer>
+		</>
+	)
+}
